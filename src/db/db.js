8e381aaa8f3d621dd5f02c175fe4b30c68e0a268
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-03-06 22:08:56 +0530

    database connection done

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const conectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `database connected : DB HOST: ${conectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.error("database connection fail", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import connectDB from "./db/db.js";
+
+connectDB();
